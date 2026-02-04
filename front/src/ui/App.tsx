912a2f463c3dc6f7743979f90eb97060aed1a3e3
Gwendal Fernet <gwendal@superg.fr>
2026-02-04 18:18:03 +0100

    ui lib

--- front/src/ui/App.tsx
+++ front/src/ui/App.tsx
@@ -1,73 +1,73 @@
 import {
-	createEffect,
-	createResource,
-	createSignal,
-	onCleanup,
-	onMount,
+  createEffect,
+  createResource,
+  createSignal,
+  onCleanup,
+  onMount,
 } from "solid-js";
 import Left from "~icons/ant-design/arrow-left-outlined";
 import Right from "~icons/ant-design/arrow-right-outlined";
 import {
-	fromDate,
-	presetMs,
-	type TimePreset,
-	TimePresets,
-	timeRangeFor,
-	timestamp,
+  fromDate,
+  presetMs,
+  type TimePreset,
+  TimePresets,
+  timeRangeFor,
+  timestamp,
 } from "../domain/TimeRange";
 import { DatePicker } from "./DatePicker";
 import { Graph } from "./Graph";
 import { useSyncer } from "./syncerContext";
 
 export const App = () => {
-	const syncer = useSyncer();
-	const [end, setEnd] = createSignal(new Date("2026-01-1"));
-	const [preset, _] = createSignal<TimePreset>(TimePresets.Last24Hours);
-	const range = () => timeRangeFor(preset(), fromDate(end()));
-	const [data, { mutate }] = createResource(range, (r) =>
-		syncer.getMeasurements(r),
-	);
+  const syncer = useSyncer();
+  const [end, setEnd] = createSignal(new Date("2026-01-1"));
+  const [preset, _] = createSignal<TimePreset>(TimePresets.Last24Hours);
+  const range = () => timeRangeFor(preset(), fromDate(end()));
+  const [data, { mutate }] = createResource(range, (r) =>
+    syncer.getMeasurements(r),
+  );
 
-	onMount(() => {
-		const unsub = syncer.onChange(async () => {
-			const udapted = await syncer.getMeasurements(range());
-			mutate(udapted);
-		});
-		onCleanup(unsub);
-	});
+  onMount(() => {
+    const unsub = syncer.onChange(async () => {
+      const udapted = await syncer.getMeasurements(range());
+      mutate(udapted);
+    });
+    onCleanup(unsub);
+  });
 
-	createEffect(() => {
-		syncer.fetchMissing(range());
-	});
+  createEffect(() => {
+    syncer.fetchMissing(range());
+  });
 
-	return (
-		<div class="h-full flex flex-col">
-			<Header preset={preset} end={end} setEnd={setEnd} />
-			<Graph
-				timeWindow={() => TimePresets.Last24Hours}
-				glucose={data}
-				class={() => "grow"}
-			/>
-		</div>
-	);
+  return (
+    <div class="h-full flex flex-col">
+      <Header preset={preset} end={end} setEnd={setEnd} />
+      <Graph
+        timeWindow={() => TimePresets.Last24Hours}
+        glucose={data}
+        class={() => "grow"}
+      />
+    </div>
+  );
 };
 
 const Header = (props: {
-	preset: () => TimePreset;
-	end: () => Date;
-	setEnd: (d: Date) => void;
+  preset: () => TimePreset;
+  end: () => Date;
+  setEnd: (d: Date) => void;
 }) => {
-	const previous = () => {
-		const end = fromDate(props.end());
-		const { from: start } = timeRangeFor(props.preset(), end);
-		props.setEnd(new Date(start));
-	};
+  const previous = () => {
+    const end = fromDate(props.end());
+    const { from: start } = timeRangeFor(props.preset(), end);
+    props.setEnd(new Date(start));
+  };
 
-	const next = () => {
-		const end = fromDate(props.end());
-		const newEnd = timestamp(end + presetMs(props.preset()));
-		props.setEnd(new Date(newEnd));
-	};
+  const next = () => {
+    const end = fromDate(props.end());
+    const newEnd = timestamp(end + presetMs(props.preset()));
+    props.setEnd(new Date(newEnd));
+  };
 
 	return (
 		<div class="flex justify-center">
